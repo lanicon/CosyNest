d22317758d4512c3b6da9a43f89a7ee57a7b14fa
xiaoyang liou <fulangxisideleike@qq.com>
2021-01-25 22:34:00 +0800

    111111111

---
+++ 通用模块/通用工具模块/通用工具模块/关于数学/关于二进制/实例类型/BinaryConvert.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Buffers;
+using System.IO.Pipelines;
+using System.Threading.Tasks;
+
+namespace System.Maths
+{
+    #region 说明文档
+    /*问：BinaryConvert和BinaryVerify是两个非常简单的委托声明，
+      它们具有什么作用？
+      答：BinaryConvert能够将二进制内容翻译成另一种格式，
+      BinaryVerify能够验证二进制内容的完整性，
+      它们通常用于安全领域，但是也可以用在其他地方，
+      因此作者决定将它们放在通用工具模块
+      
+      问：如何使用BinaryConvert表示对称加密，非对称加密和哈希函数？
+      答：对于对称加密和非对称加密，
+      使用两个BinaryConvert分别表示加密和解密转换，密钥则自包含在委托中，
+      至于哈希函数，仅使用一个BinaryConvert即可*/
+    #endregion
+    #region 读取并转换管道
+    /// <summary>
+    /// 读取并转换一个管道的内容
+    /// </summary>
+    /// <param name="reader">待读取的管道</param>
+    /// <returns>一个新的管道，通过它可以读取转换后的二进制数据</returns>
+    public delegate Task<PipeReader> BinaryConvert(PipeReader reader);
+    #endregion
+    #region 读取并验证管道
+    /// <summary>
+    /// 读取并验证一个管道的内容
+    /// </summary>
+    /// <param name="reader">待读取的管道</param>
+    /// <param name="comparison">用来和管道内容进行对比的数据</param>
+    /// <returns>如果验证通过，则为<see langword="true"/>，否则为<see langword="false"/></returns>
+    public delegate Task<bool> BinaryVerify(PipeReader reader, ReadOnlySequence<byte> comparison);
+    #endregion
+}
---
+++ 通用模块/通用工具模块/通用工具模块/关于数学/静态类型/ExtenMath.cs
@@ -0,0 +1,31 @@
+﻿using System.IO.Pipelines;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Maths
+{
+    /// <summary>
+    /// 有关数学的静态类
+    /// </summary>
+    public static class ExtenMath
+    {
+        #region 有关二进制转换
+        #region 二进制转换字符串
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="convert"></param>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static async Task<string> ConvertText(this BinaryConvert convert, string text)
+        {
+            var arry = Encoding.Unicode.GetBytes(text);
+            var pipe = new Pipe();
+            await pipe.Writer.WriteAsync(arry);
+            await pipe.Writer.CompleteAsync();
+            var results = await convert(pipe.Reader);
+        }
+        #endregion
+        #endregion
+    }
+}
