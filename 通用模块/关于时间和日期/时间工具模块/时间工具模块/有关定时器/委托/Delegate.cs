5f3933be94c8227013700bf3a43de7e6348e3a88
xiaoyang liou <fulangxisideleike@qq.com>
2021-02-06 20:51:01 +0800

    11111111

--- 通用模块/关于时间和日期/时间工具模块/时间工具模块/有关定时器/委托/Delegate.cs
+++ 通用模块/关于时间和日期/时间工具模块/时间工具模块/有关定时器/委托/Delegate.cs
@@ -2,14 +2,6 @@
 
 namespace System.Time
 {
-    /// <summary>
-    /// 获取从某个时间点开始，下一次执行定时器的时间
-    /// </summary>
-    /// <param name="Date">当前时间，如果为<see langword="null"/>，
-    /// 默认为<see cref="DateTimeOffset.MinValue"/>，等同于计算第一次执行定时器的时间</param>
-    /// <returns>下一次执行定时器的时间，如果为<see langword="null"/>，表示定时器已终止</returns>
-    public delegate DateTimeOffset? NextDate(DateTimeOffset? Date = null);
-
     #region 说明文档
     /*问：该委托与IPlanTrigger有何区别？
       答：本委托假设执行定时器的时候，.net进程仍然处于运行状态，
@@ -19,4 +11,13 @@ namespace System.Time
       为了保证兼容，IPlanTriggerTiming也有一个NextDate方法，
       它的签名和本委托相同，可以直接赋值给这个委托*/
     #endregion
+    #region 获取下一次执行时间的委托
+    /// <summary>
+    /// 获取从某个时间点开始，下一次执行定时器的时间
+    /// </summary>
+    /// <param name="Date">当前时间，如果为<see langword="null"/>，
+    /// 默认为<see cref="DateTimeOffset.MinValue"/>，等同于计算第一次执行定时器的时间</param>
+    /// <returns>下一次执行定时器的时间，如果为<see langword="null"/>，表示定时器已终止</returns>
+    public delegate DateTimeOffset? NextDate(DateTimeOffset? Date = null);
+    #endregion 
 }
