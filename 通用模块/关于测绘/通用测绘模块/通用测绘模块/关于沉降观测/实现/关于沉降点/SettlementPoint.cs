abdef3b793867f0ccd4282ec036d6d36501bbd69
xiaoyang liou <fulangxisideleike@qq.com>
2021-03-18 21:25:13 +0800

    11111111

---
+++ 通用模块/关于测绘/通用测绘模块/通用测绘模块/关于沉降观测/实现/关于沉降点/SettlementPoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Maths;
+
+namespace System.Mapping.Settlement
+{
+    /// <summary>
+    /// 这个类型代表一个沉降观测点
+    /// </summary>
+    class SettlementPoint : SettlementPointBase
+    {
+        #region 高程
+        public override IUnit<IUTLength> High => throw new NotImplementedException();
+        #endregion
+        #region 构造函数
+        /// <inheritdoc cref="SettlementPointBase(string, IUnit{IUTLength})"/>
+        /// <param name="Father">上一站观测点的名称</param>
+        public SettlementPoint(string Name, IUnit<IUTLength> HighOriginal, INode? Father)
+            : base(Name, HighOriginal)
+        {
+            this.Father = Father;
+        }
+        #endregion
+    }
+}
---
+++ 通用模块/关于测绘/通用测绘模块/通用测绘模块/关于沉降观测/实现/关于沉降点/SettlementPointRoot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Maths;
+
+namespace System.Mapping.Settlement
+{
+    /// <summary>
+    /// 代表一个专门用来作为起始点的沉降点，
+    /// 它储存了有关已知固定高程点的信息
+    /// </summary>
+    class SettlementPointRoot : SettlementPointFixed
+    {
+        #region 索引高程已知的沉降点
+        /// <summary>
+        /// 索引本次沉降观测中，
+        /// 高程已知的点的名称的高程
+        /// </summary>
+        public IReadOnlyDictionary<string, IUnit<IUTLength>> Known { get; }
+        #endregion
+        #region 构造函数
+        /// <inheritdoc cref="SettlementPointFixed(string, IUnit{IUTLength})"/>
+        /// <param name="Known">索引本次沉降观测中，高程已知的点的名称的高程</param>
+        public SettlementPointRoot(string Name, IUnit<IUTLength> High, IReadOnlyDictionary<string, IUnit<IUTLength>> Known)
+            : base(Name, High)
+        {
+            this.Known = Known;
+        }
+        #endregion
+    }
+}
