0e7b3283d25273e9a6de38285b250db54ae6c243
xiaoyang liou <fulangxisideleike@qq.com>
2021-04-02 22:07:03 +0800

    111111

--- 测试项目/测试/测试控制台/Program.cs
+++ 测试项目/测试/测试控制台/Program.cs
@@ -1,23 +1,5 @@
 ﻿#pragma warning disable
 
 using System;
-using System.Linq;
-using System.Mapping;
-using System.Mapping.Settlement;
-using System.Maths;
 
-
-
-var a = CreateMapping.SettlementPointRoot("BM1", 21);
-var B = a.Add(11111).Add("1", 22222).Add(33333).Add("2", 44444).Add(55555).
-    Add("3", 66666).Add(77777);
-B.Add("BM1", 88888);
-B.Father.RemoveOffspring();
-var x = a.SonAll.OfType<ISettlementPoint>().ToArray();
-foreach (var item in x)
-{
-    Console.WriteLine($"{item.Name}:{item.High}:{item.ClosedDifference}");
-}
-
-Console.ReadLine();
 ;
